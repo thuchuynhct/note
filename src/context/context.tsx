345cfeb1fc7d0a43ae8218ebf7b53254876ae29c
thuchuynh97 <thuchuynh9711@gmail.com>
2022-11-28 10:33:42 +0700

    sua localStorage

--- src/context/context.tsx
+++ src/context/context.tsx
@@ -9,11 +9,11 @@ const initialNotes: NoteType[] = [];
 const AppContext = createContext<{ state: NoteType[], dispatch: React.Dispatch<any> }>({ state: initialNotes, dispatch: () => null });
 
 const AppProvider = ({ children }: { children: JSX.Element }) => {
-    const saved = localStorage.getItem("notes") ?? "s";
-    const [state, dispatch] = useReducer(reducers, JSON.parse(saved) ?? initialNotes);
+    const saved = localStorage.getItem("notes");
+    const [state, dispatch] = useReducer(reducers, saved && JSON.parse(saved) || initialNotes);
     useEffect(() => {
         localStorage.setItem("notes", JSON.stringify(state));
-       // console.log(JSON.parse(saved), "Josn")
+        // console.log(JSON.parse(saved), "Josn")
     }, [state])
 
     return (
